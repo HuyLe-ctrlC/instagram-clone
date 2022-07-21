171bac30acebfbca1a4ee50716daf80963de21db
Huy Le Minh <intern.leminhhuy@tma.com.vn>
2022-07-21 13:54:26 +0700

    update office 21/07

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,9 @@
 import React, { useContext, useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import FirebaseContext from "./../context/firebase";
 import LogoIPhone from "../images/iphoneWithProfile.jpg";
 import logo from "../images/logo.png";
+import { SIGN_UP } from "./../constants/routes";
 
 export default function Login() {
   const navigate = useNavigate();
@@ -22,35 +23,108 @@ export default function Login() {
 
   return (
     <div className="container flex mx-auto max-w-screen-md items-center h-screen">
-      <div className="flex w-3/5">
+      <div className="flex w-3/5 md:w-3/5 sm:w-0 lg:w-3/5">
         <img src={LogoIPhone} alt="Iphone with Instagram" />
       </div>
-      <div className="flex flex-col w-2/5">
-        <h1 className="flex justify-center">
-          <img src={logo} alt="logo" className="mt-2 mb-4 w-6/12" />
-        </h1>
-        {error && <p className="mb-4 text-xs text-red-primary ">{error}</p>}
-        <form onSubmit={handleLogin} method="POST">
-          <input
-            aria-label="Enter your email address"
-            type="text"
-            placeholder="Email address"
-            className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
-            onChange={({ target }) => console.log(target.value)}
-          />
-          <input
-            aria-label="Enter your Password"
-            type="password"
-            placeholder="Password"
-            className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
-            onChange={({ target }) => console.log(target.value)}
-          />
-        </form>
+      <div className="flex flex-col items-center border border-gray-primary bg-white p-4 mb-4 w-2/5 md:w-2/5 sm:w-full sm:m-10 lg:w-3/5 rounded">
+        <div className="flex flex-col w-full">
+          <h1 className="flex justify-center">
+            <img src={logo} alt="logo" className="mt-2 mb-4 w-6/12" />
+          </h1>
+          {error && <p className="mb-4 text-xs text-red-primary ">{error}</p>}
+          <form onSubmit={handleLogin} method="POST">
+            <span class="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
+              Email
+            </span>
+            <input
+              aria-label="Enter your email address"
+              type="text"
+              placeholder="Email address"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              onChange={({ target }) => setEmailAddress(target.value)}
+            />
+            <span class="after:content-['*'] after:ml-0.5 after:text-red-500 block text-sm font-medium text-slate-700">
+              Password
+            </span>
+            <input
+              aria-label="Enter your Password"
+              type="password"
+              placeholder="Password"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+
+            <button
+              disabled={isInvalid}
+              type="submit"
+              className={`text-white bg-gradient-to-br from-pink-500 to-orange-400 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 w-full ${
+                isInvalid && " opacity-50"
+              }`}
+            >
+              Login
+            </button>
+            <div className="flex flex-col">
+              <button
+                type="button"
+                className="text-white bg-[#3b5998] hover:bg-[#3b5998]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#3b5998]/55 mr-2 mb-2 flex-1 w-full"
+              >
+                <svg
+                  className="mr-2 -ml-1 w-4 h-4"
+                  aria-hidden="true"
+                  focusable="false"
+                  data-prefix="fab"
+                  data-icon="facebook-f"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 320 512"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M279.1 288l14.22-92.66h-88.91v-60.13c0-25.35 12.42-50.06 52.24-50.06h40.42V6.26S260.4 0 225.4 0c-73.22 0-121.1 44.38-121.1 124.7v70.62H22.89V288h81.39v224h100.2V288z"
+                  ></path>
+                </svg>
+                Sign in with Facebook
+              </button>
+              <button
+                type="button"
+                className="text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 mr-2 mb-2 flex-1 w-full"
+              >
+                <svg
+                  className="mr-2 -ml-1 w-4 h-4"
+                  aria-hidden="true"
+                  focusable="false"
+                  data-prefix="fab"
+                  data-icon="google"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 488 512"
+                >
+                  <path
+                    fill="currentColor"
+                    d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
+                  ></path>
+                </svg>
+                Sign in with Google
+              </button>
+            </div>
+          </form>
+        </div>
+        <div className="flex flex-col justify-center items-center bg-white rounded border border-gray-primary w-full h-10">
+          <div className="text-sm">
+            Dont't have an account? {""}
+            <Link to={SIGN_UP} className="font-bold text-blue-media">
+              Sign up
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
 }
 
+//TODO: add Tailwind config
+// -> bg-blue-medium => hex values
 //-> text-red-primary => hex values
+// -> text-blue-media => hex values
 // -> text-gray-base => hex values
 // -> border-gray-primary => hex values
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,45 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      white: "#ffffff",
+      blue: {
+        medium: "#005C98",
+      },
+      black: {
+        light: "#005C98",
+        fade: "#00000059",
+      },
+      gray: {
+        base: "#616161",
+        background: "#fafafa",
+        primary: "#dbdbdb",
+      },
+      pink: {
+        50: "#fdf2f8",
+        100: "#fce7f3",
+        200: "#fbcfe8",
+        300: "#f9a8d4",
+        400: "#f472b6",
+        500: "#ec4899",
+        600: "#db2777",
+        700: "#be185d",
+        800: "#9d174d",
+        900: "#831843",
+      },
+      orange: {
+        50: "#fff7ed",
+        100: "#ffedd5",
+        200: "#fed7aa",
+        300: "#fdba74",
+        400: "#fb923c",
+        500: "#f97316",
+        600: "#ea580c",
+        700: "#c2410c",
+        800: "#9a3412",
+        900: "#7c2d12",
+      },
+    },
   },
   plugins: [],
 };
